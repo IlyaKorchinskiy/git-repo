f1f534b03cf31b45aff739fd4482d22fc0031e0d
IlyaKorchinskiy <korchinskiy.ilya@gmail.com>
2018-07-07 12:00:50 +0300

    добавил определение ячейки

--- JavaCoreProjectAdvanced/src/lesson2/MyArrayDataException.java
+++ JavaCoreProjectAdvanced/src/lesson2/MyArrayDataException.java
@@ -1,4 +1,16 @@
 package lesson2;
 
 public class MyArrayDataException extends Exception {
+
+    private int x;
+    private int y;
+
+    public MyArrayDataException(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    void printMessage() {
+        System.out.println("Ошибка! В ячейке " + x + "|" + y + " неверные данные!");
+    }
 }
